bf8d3532575ea8864d1d6af0893bbcef2b1c7de8
harindernirman <39080440+harindernirman@users.noreply.github.com>
2024-01-01 14:53:32 +0530

    gf78

--- pages/index.js
+++ pages/index.js
@@ -15,30 +15,10 @@ export default function Home() {
         </h1>
 
         <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
+         Harinder Nirmanaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
         </p>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-        
-        </div>
+      
       </main>
 
       <footer>
